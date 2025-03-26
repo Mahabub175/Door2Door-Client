09db38e2a3a148b3d2573346de4757bf10aa5f1f
Mahabub175 <rgrxmahabub.official@gmail.com>
2025-03-26 16:10:19 +0600

    login page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
       const res = await login(data).unwrap();
       const user = verifyToken(res.access);
       dispatch(setUser({ user: user, token: res.access }));
-      toast.success("Logged in successfully!", { id: toastId, duration: 2000 });
+      toast.success("Logged in successfully!", { id: toastId });
       navigate("/dashboard");
     } catch (error) {
       toast.error("Invalid credentials. Please try again!", {
@@ -31,13 +31,30 @@ const Login = () => {
   };
 
   return (
-    <>
-      <div className="h-screen flex justify-center items-center bg-excel/10">
-        <div className="lg:w-[500px] bg-white rounded-3xl p-10">
-          <h1 className="text-center mb-10 text-5xl font-extrabold">Log In</h1>
+    <section
+      style={{
+        backgroundImage:
+          "url('https://viscarttoolsapi.moonsgallerysystem.com/uploads/1742983046581-loginBg.png')",
+      }}
+      className="relative bg-cover bg-center bg-no-repeat h-full"
+    >
+      <div className="absolute inset-0 bg-white opacity-70"></div>
+
+      <div className="relative h-screen flex justify-center items-center">
+        <div className="hidden lg:block lg:w-[550px]">
+          <img src={"/login.png"} alt="login" width={500} height={500} />
+        </div>
+        <div className="lg:w-[550px] p-10 text-center lg:text-start">
+          <h1 className="mb-4 text-xl md:text-4xl font-semibold">
+            Login To Your Account
+          </h1>
+          <h3 className="mb-5 text-gray-600 text-sm md:text-base">
+            Login now to access real-time delivery tracking, seamless logistics
+            management, and secure shipments!
+          </h3>
           <CustomForm onSubmit={onSubmit} className="flex flex-col gap-6">
             <CustomInput
-              label="Email or Username"
+              label="Email"
               type={"text"}
               required={true}
               name={"email"}
@@ -53,7 +70,7 @@ const Login = () => {
             <Button
               htmlType="submit"
               loading={isLoading}
-              className="font-bold w-full pt-2 pb-8 rounded-full"
+              className="font-bold w-full pt-2 pb-8 rounded-lg"
               type="primary"
             >
               Enter
@@ -61,7 +78,7 @@ const Login = () => {
           </CustomForm>
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
